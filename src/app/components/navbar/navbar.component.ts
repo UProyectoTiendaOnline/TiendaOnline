2aa32b9bc1c04585705cd63b2a4ea89a66e50715
Alan Sanchez <sanchezpineda03@gmail.com>
2020-01-29 23:48:20 -0600

    Componente para la barra de navegación
    Estilos y estructura básica del componente, así como
    comportamiento en javascript para el dropdown de acciones de usuario.
    
    TODO:
    - Incorporar iconos adecuados para la navegación,
      por el momento todos usan .fa-home
    - Implementar toggle para el menú en dispositivos móviles
    - Estilos para botones de registrar/iniciar sesión. Por el
      momento solo se encuentran los estilos para el caso de
      un usuario registrado
    - Implementar lógica para ngIf en caso de una sesión iniciada
    - Verificar que el menu (móvil) y el dropdown (desktop) se
      oculten al cambiar de sección en el router.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+    Component, 
+    Renderer2, 
+    OnDestroy, 
+    ViewChild, 
+    ElementRef,
+    AfterViewInit 
+} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class NavbarComponent implements AfterViewInit, OnDestroy {
+  widthBreakdown: MediaQueryList;
+  clickListener;
+  touchListener;
+ 
+  @ViewChild('dropdownMenu', {static: false}) dropdownMenu; 
+  constructor(private renderer: Renderer2) {
+      this.widthBreakdown = window.matchMedia('(min-width: 992px)');
   }
 
+  toggleNavbar() {
+
+  }
+
+  checkWindowWidth = () => {
+    if (this.widthBreakdown.matches) {
+        this.useDesktopMenu();
+    } else {
+        this.useMobileMenu();
+    }
+  }
+
+  eventHandler = (e) => {
+    if ( 
+        !this.dropdownMenu.nativeElement.contains(e.target) &&
+        this.dropdownMenu.nativeElement.classList.contains('show')
+      ) {
+      this.dropdownMenu.nativeElement.classList.remove('show');
+    }
+    else if (
+        e.target.parentElement.matches('.navigation__account-actions') &&
+        !this.dropdownMenu.nativeElement.classList.contains('show')
+      ) {
+      this.dropdownMenu.nativeElement.classList.add('show');
+    }
+  }
+
+  useDesktopMenu() {
+      this.clickListener = this.renderer
+          .listen('document', 'click', this.eventHandler);
+      this.touchListener = this.renderer
+          .listen('document', 'touchstart', this.eventHandler);
+  }
+
+  useMobileMenu() {
+      if (this.clickListener) {
+          this.clickListener()
+      }
+      if (this.touchListener) {
+          this.touchListener()
+      }
+  }
+
+  ngAfterViewInit() {
+      if (this.widthBreakdown.matches) {
+          this.useDesktopMenu();
+      }
+      
+      this.widthBreakdown.addListener(this.checkWindowWidth);
+  }
+
+  ngOnDestroy() {
+      if (this.clickListener) {
+          this.clickListener()
+      }
+      if (this.touchListener) {
+          this.touchListener()
+      }
+      this.widthBreakdown.removeListener(this.checkWindowWidth);
+  }
 }
