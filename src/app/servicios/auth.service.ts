5d74844a204891da30afac54f2664ead6467630b
Alan Sanchez <sanchezpineda03@gmail.com>
2020-02-10 18:04:33 -0600

    Servicio de autenticación: Redirección posterior a login y registro, Observable de usuario

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,49 +1,77 @@
 import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+
 import { map} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Injectable()
 export class AuthService {
+  private _user = new Subject<any>();
+  user = this._user.asObservable();
 
   constructor(
-    public afAuth: AngularFireAuth
-  ) { }
+    private _firebaseAuth: AngularFireAuth,
+    private _router: Router
+  ) {
+    this._firebaseAuth.authState.subscribe(user => {
+      let dataToSave = (user) ? JSON.stringify(user) : null;
+      this._user.next(user);
 
-  loginTwitter () {
-    return this.afAuth.auth.signInWithPopup( new firebase.auth.TwitterAuthProvider());
-  }
-
-  loginFacebook() {
-    return this.afAuth.auth.signInWithPopup( new firebase.auth.FacebookAuthProvider());
-  }
-
-  loginGoogle() {
-    return this.afAuth.auth.signInWithPopup( new firebase.auth.GoogleAuthProvider());
-  }
-
-  registerUser(email: string, pass: string) {
-    return new Promise((resolve, reject) => {
-      this.afAuth.auth.createUserWithEmailAndPassword(email, pass)
-      .then( userData =>  resolve(userData),
-      err => reject (err));
+      localStorage.setItem('user', dataToSave);
+      JSON.parse(localStorage.getItem('user'));
     });
   }
 
-  loginEmail(email: string, pass: string) {
+  isLoggedIn() {
+    if (this.user == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  loginTwitter() {
+    return this._firebaseAuth.auth.signInWithPopup( new firebase.auth.TwitterAuthProvider());
+  }
+
+  loginFacebook() {
+    return this._firebaseAuth.auth.signInWithPopup( new firebase.auth.FacebookAuthProvider());
+  }
+
+  loginGoogle() {
+    return this._firebaseAuth.auth.signInWithPopup( new firebase.auth.GoogleAuthProvider());
+  }
+
+  registerUser(email: string, pass: string, redirectTo: string = '/') {
     return new Promise((resolve, reject) => {
-      this.afAuth.auth.signInWithEmailAndPassword(email, pass)
-      .then( userData =>  resolve(userData),
-      err => reject (err));
+      this._firebaseAuth.auth.createUserWithEmailAndPassword(email, pass)
+      .then(userData => {
+          this._router.navigate([redirectTo]);
+          resolve(userData);
+        }, err => reject (err));
+    });
+  }
+
+  loginEmail(email: string, pass: string, redirectTo: string = '/') {
+    return new Promise((resolve, reject) => {
+      this._firebaseAuth.auth.signInWithEmailAndPassword(email, pass)
+        .then(userData => {
+          this._router.navigate([redirectTo]);
+          resolve(userData);
+        }, err => reject (err));
     });
   }
 
   getAuth() {
-    return this.afAuth.authState.pipe(map(auth => auth));
+    return this._firebaseAuth.authState.pipe(map(auth => auth));
   }
 
   logout() {
-    return this.afAuth.auth.signOut();
+    return this._firebaseAuth.auth.signOut()
+      .then((res) => this._router.navigate(['/']));
   }
 
 }
