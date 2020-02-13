551e79ae1b3c27f3546493b0c181bded66e5175a
Alan Sanchez <sanchezpineda03@gmail.com>
2020-02-12 19:12:15 -0600

    Implementación de clase css content-wrapper en componente de homepage.

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+  host: {
+      class: 'content-wrapper'
+  }
 })
 export class HomePageComponent implements OnInit {
 
